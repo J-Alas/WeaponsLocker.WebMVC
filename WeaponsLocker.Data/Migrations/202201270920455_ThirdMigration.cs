83319fe17ca93c80358ab76bd9477413b70390c3
Jon Alas <jalas1218@gmail.com>
2022-01-27 03:22:03 -0600

    update

---
+++ WeaponsLocker.Data/Migrations/202201270920455_ThirdMigration.cs
@@ -0,0 +1,20 @@
+﻿namespace WeaponsLocker.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ThirdMigration : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Firearms", "GunModel", c => c.String(nullable: false));
+            DropColumn("dbo.Firearms", "Model");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Firearms", "Model", c => c.String(nullable: false));
+            DropColumn("dbo.Firearms", "GunModel");
+        }
+    }
+}
